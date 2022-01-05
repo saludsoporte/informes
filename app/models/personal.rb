03fcfaf3b4f130d272404ced3b61e8fecee25640
saludsoporte <salud.aplicaciones@gmail.com>
2022-01-05 13:34:26 -0600

    se acomodan todos los datos nomrales

--- app/models/personal.rb
+++ app/models/personal.rb
@@ -2,4 +2,8 @@ class Personal < ApplicationRecord
     #belongs_to :user
     has_one :user
     self.per_page = 10
+
+    def nombre_completo
+        (self.nombre+" "+self.apellido_p+" "+self.apellido_m).titleize
+    end
 end
